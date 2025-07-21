3f34ad79bf16cceaf7d835238191dcb32e0b63ff
Abdullah <abdullah.work027@gmail.com>
2025-07-21 21:38:46 +0500

    Added download functionality

--- websites/views.py
+++ websites/views.py
@@ -1,24 +1,28 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render
 from django.http import HttpResponse
 from .forms import WebsiteForm
 import qrcode
-
+import base64
 from io import BytesIO
 
-# Create your views here.
-
 def home(request):
-    image = None
+    qr_image_base64 = None
     form = WebsiteForm()
+    
     if request.method == "POST":
         form = WebsiteForm(request.POST)
         if form.is_valid():
             data = form.cleaned_data['name']
             qr = qrcode.make(data)
             buffer = BytesIO()
-            qr.save(buffer, format = 'PNG')
-            return HttpResponse(buffer.getvalue(), content_type='image/png')
-    context = {'form': form}
-    return render(request, 'websites/home.html', context)
+            qr.save(buffer, format='PNG')
 
-    
+            # Rewind the buffer before reading
+            buffer.seek(0)
+
+            # Encode to base64
+            img_base64 = base64.b64encode(buffer.read()).decode('utf-8')
+            qr_image_base64 = f'data:image/png;base64,{img_base64}'
+
+    context = {'form': form, 'qr_image': qr_image_base64}
+    return render(request, 'websites/home.html', context)
