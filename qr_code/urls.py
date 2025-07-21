e8a23b3417483300bbd04f2859ab0f6ad6679a7b
Abdullah <abdullah.work027@gmail.com>
2025-07-21 21:27:47 +0500

    Initial Commit

--- qr_code/urls.py
+++ qr_code/urls.py
@@ -7,8 +7,3 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('websites.urls'))
 ]
-
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
